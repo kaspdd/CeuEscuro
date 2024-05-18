83fa30f26de364cb7fc869b96bbfdfed11e3c74e
kaspdd <carlos.souza.pdc@gmail.com>
2024-05-17 22:10:29 -0300

    finalizado o filme.bll

--- CeuEscuro.BLL/FilmeBLL.cs
+++ CeuEscuro.BLL/FilmeBLL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CeuEscuro.DTO;
+using CeuEscuro.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,54 @@ using System.Threading.Tasks;
 
 namespace CeuEscuro.BLL
 {
-    internal class FilmeBLL
+    public class FilmeBLL :
     {
+        FilmDAL objBLL = new FilmDAL();
+
+        public void Create(FilmDTO filme)
+        {
+            objBLL.Create(filme);
+        }
+
+        public List<FilmDTO> GetFilms()
+        {
+            return objBLL.GetFilms();
+        }
+
+        public void Update(FilmDTO filme)
+        {
+            objBLL.Update(filme);
+        }
+
+        public void Delete(int idFilme)
+        {
+            objBLL.Delete(idFilme);
+        }
+
+        public FilmDTO SearchByID(int filmeId)
+        {
+            return objBLL.SearchByID(filmeId);
+        }
+
+        public FilmDTO SearchByName(string filmeTitulo)
+        {
+            return objBLL.SearchByName(filmeTitulo);
+        }
+
+        public List<FilmDTO> Filter(string generoFilme)
+        {
+            return objBLL.Filter(generoFilme);
+        }
+
+        public List<ClassificacaoDTO> LoadDLLClassificacao()
+        {
+            return objBLL.LoadDLLClassificacao();
+        }
+
+        public List<GeneroDTO> LoadDLLGenero()
+        {
+           return objBLL.LoadDLLGenero();
+        }
+
     }
 }
